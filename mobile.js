6932eee4b3904b5f8f8f773f7331d9f95bded67b
myeongyunChoi <104118233+myeongyunChoi@users.noreply.github.com>
2023-07-09 01:41:35 +0900

    mobile-card initial

---
+++ mobile.js
@@ -0,0 +1,20 @@
+
+// const canvas = document.getElementById("myCanvas");
+// const ctx = canvas.getContext("2d");
+// const img = new Image();
+
+// img.onload = () => {
+//     canvas.width = img.width;
+//     canvas.height = img.height;
+
+//     ctx.drawImage(img, 0, 0);
+
+//     const gradient = ctx.createLinearGradient(0, canvas.height * 0.7, 0, canvas.height);
+//     gradient.addColorStop(0, "rgba(0, 0, 0, 0)");
+//     gradient.addColorStop(0, "rgba(0, 0, 0, 1)");
+
+//     ctx.fillStyle = gradient;
+//     ctx.fillRect(0, canvas.height * 0.7, canvas.width, canvas.height);
+// };
+
+// img.src = "img/test.jpg";
